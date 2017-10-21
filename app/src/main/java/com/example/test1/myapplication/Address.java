0e5969900c7e273eac90e566a737c5a777dc9af1
Herman Saini <herman.saini@gmail.com>
2017-10-21 12:03:02 -0700

    boi

--- app/src/main/java/com/example/test1/myapplication/Address.java
+++ app/src/main/java/com/example/test1/myapplication/Address.java
@@ -5,4 +5,21 @@ package com.example.test1.myapplication;
  */
 
 public class Address {
+    String zipCode;
+    String city;
+    String state;
+    String streetAddress;
+
+    public Address(String a, String b, String c, String d){
+        zipCode = a;
+        city = b;
+        state = c;
+        streetAddress = d;
+    }
+
+    public void result(String duration){
+        String answer = "";
+        answer = "It takes "+Double.parseDouble(duration)/3600+" hours";
+    }
+
 }
