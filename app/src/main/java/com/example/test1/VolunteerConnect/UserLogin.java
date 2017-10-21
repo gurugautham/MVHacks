118f33f171d152b5bcc3d93a3d1a96ea46105efa
SidMajeti <sid.majeti@gmail.com>
2017-10-21 14:02:39 -0700

    Commit

--- app/src/main/java/com/example/test1/VolunteerConnect/UserLogin.java
+++ app/src/main/java/com/example/test1/VolunteerConnect/UserLogin.java
@@ -43,7 +43,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class UserLogin extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class    UserLogin extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
      * Id to identity READ_CONTACTS permission request.
