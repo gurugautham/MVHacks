5afde18b89baf3a3819f5ae200e3b7cb83e639fd
test1 <zhangtom54321@gmail.com>
2017-10-21 09:59:50 -0700

    commit

--- app/src/main/java/com/example/test1/myapplication/MainActivity.java
+++ app/src/main/java/com/example/test1/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.test1.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     // Test commit
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void doStart(View v) {
+        Button b = (Button)(Button)findViewById(R.id.button);
+        b.setText("Triggered");
     }
 }
