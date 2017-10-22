a39e586f76256841ba07fecc5f1ac19a32505571
test1 <zhangtom54321@gmail.com>
2017-10-21 19:48:40 -0700

    commit

--- app/src/main/java/com/example/test1/VolunteerConnect/EventMap.java
+++ app/src/main/java/com/example/test1/VolunteerConnect/EventMap.java
@@ -163,7 +163,7 @@ Event [] eventArray;
     public void onInfoWindowClick(Marker m) {
         for (int c = 0; c < eventArray.length; c++) {
             if (m.getTitle().equals(eventArray[c].getName())){
-                eventArray[c].setPeopleRequired(eventArray[c].getPeopleRequired()+1);
+                eventArray[c].setPeopleRequired(eventArray[c].getPeopleRequired()-1);
             }
         }
         Toast.makeText(EventMap.this,"You successfully joined this event",Toast.LENGTH_LONG).show();
