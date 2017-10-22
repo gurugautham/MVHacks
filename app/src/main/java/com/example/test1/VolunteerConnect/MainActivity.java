c838d339d1c1290fadbd367a132ce74b171462bb
Gautham Raghupathi <gauthamar11@gmail.com>
2017-10-21 19:13:07 -0700

    commit

--- app/src/main/java/com/example/test1/VolunteerConnect/MainActivity.java
+++ app/src/main/java/com/example/test1/VolunteerConnect/MainActivity.java
@@ -4,15 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
     // Test commit
+
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle("VolunteerConnect");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initializing firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
 
     protected void logIn(View v) {
@@ -24,5 +32,24 @@ public class MainActivity extends AppCompatActivity {
     protected void signUp(View v) {
         Intent myIntent = new Intent(MainActivity.this, UserSignUp.class);
         MainActivity.this.startActivity(myIntent);
+    }
+
+    protected void map(View v){
+        if(firebaseAuth.getCurrentUser()==null){
+            Toast.makeText(this, "Please login in above first!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            finish();
+            startActivity(new Intent(getApplicationContext(), EventMap.class));
+        }
+    }
+
+    protected void list(View v){
+        if(firebaseAuth.getCurrentUser()==null){
+            Toast.makeText(this, "Please login in above first!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            startActivity(new Intent(getApplicationContext(), EventMap.class));
+        }
     }
 }
